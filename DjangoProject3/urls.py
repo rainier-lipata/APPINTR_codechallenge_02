a9c5236b477946ca8c48a0c57721268c6a89df65
Ann <annmikaela.gutierrez@benilde.edu.ph>
2026-01-28 18:28:42 +0800

    updated urls

--- DjangoProject3/urls.py
+++ DjangoProject3/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Codechallenge02 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
+
+
